b82811a16cf6980a7072a5f1c479583ef2eab68b
hansmu <hans.miikael@gmail.com>
2017-01-31 19:40:35 +0200

    Get for single book and all books.

--- src/main/java/ee/ttu/spring/rest/api/BookController.java
+++ src/main/java/ee/ttu/spring/rest/api/BookController.java
@@ -3,18 +3,27 @@ package ee.ttu.spring.rest.api;
 import ee.ttu.spring.rest.domain.Book;
 import ee.ttu.spring.rest.engine.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value="book")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value="/test", method= RequestMethod.GET)
-    public Book test() {
-        return bookService.getBook();
+    @RequestMapping(value="{bookId}", method=RequestMethod.GET)
+    public Book getBookById(@PathVariable(value="bookId")Long bookId) {
+        return bookService.getBook(bookId);
+    }
+
+    @RequestMapping(value="get-all", method=RequestMethod.GET)
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
     }
 }
--- src/main/java/ee/ttu/spring/rest/engine/BookService.java
+++ src/main/java/ee/ttu/spring/rest/engine/BookService.java
@@ -5,13 +5,19 @@ import ee.ttu.spring.rest.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
 
-    public Book getBook() {
-        return bookRepository.findOne(1L);
+    public Book getBook(Long bookId) {
+        return bookRepository.findOne(bookId);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
