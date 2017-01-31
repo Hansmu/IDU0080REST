4180829c3edba7342488dee181860cf9744170dc
hansmu <hans.miikael@gmail.com>
2017-01-31 21:09:39 +0200

    Fixed auto incrementing in SQL.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE book
 (
-  id BIGINT PRIMARY KEY,
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
   book_title VARCHAR2(250),
   genre VARCHAR2(250),
   page_count INTEGER
@@ -8,7 +8,7 @@ CREATE TABLE book
 
 CREATE TABLE authors
 (
-  id BIGINT PRIMARY KEY,
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
   authors_names VARCHAR2(250),
   book_id BIGINT,
   FOREIGN KEY (book_id) REFERENCES book(id)
