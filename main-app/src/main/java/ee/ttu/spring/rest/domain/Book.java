91720a91c07345b66ed7760b6d20bd6f39cc93dd
Maksim Lind <maksli@ttu.ee>
2017-02-04 12:51:35 +0200

    Remove boilerplate code from Book class
    Used lombok to remove default getters and setters.

--- main-app/src/main/java/ee/ttu/spring/rest/domain/Book.java
+++ main-app/src/main/java/ee/ttu/spring/rest/domain/Book.java
@@ -2,9 +2,10 @@ package ee.ttu.spring.rest.domain;
 
 import javax.persistence.*;
 import java.util.List;
+import lombok.Data;
 
 @Entity
-public class Book {
+public @Data class Book {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -19,44 +20,4 @@ public class Book {
     private String genre;
 
     private int pageCount;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<String> getAuthorsNames() {
-        return authorsNames;
-    }
-
-    public void setAuthorsNames(List<String> authorsNames) {
-        this.authorsNames = authorsNames;
-    }
-
-    public String getBookTitle() {
-        return bookTitle;
-    }
-
-    public void setBookTitle(String bookTitle) {
-        this.bookTitle = bookTitle;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getPageCount() {
-        return pageCount;
-    }
-
-    public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
 }
