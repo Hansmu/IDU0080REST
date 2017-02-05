1dec2247503ae8fa85ba733ff198087e3e6af536
hansmu <hans.miikael@gmail.com>
2017-02-05 18:56:00 +0200

    Additional book in initial data to test searching.

--- main-app/src/main/resources/data.sql
+++ main-app/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 MERGE INTO book (id, authors_names, book_title, genre, page_count) VALUES (1, 'Bob Roberts, Michael J. Flackson', 'Greatest Book', 'Mystery', 273);
 MERGE INTO book (id, authors_names,book_title, genre, page_count) VALUES (2, 'John J. Johnathan', 'Also Good Book', 'Adventure', 353);
-
+MERGE INTO book (id, authors_names,book_title, genre, page_count) VALUES (3, 'John J. Johnathan', 'The Goodest Book Of Johnny', 'Mystery', 412);
