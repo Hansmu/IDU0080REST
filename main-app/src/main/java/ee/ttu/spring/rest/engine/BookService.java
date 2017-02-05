0e1c42a8cd4187c3832042404657e6e6280ad471
hansmu <hans.miikael@gmail.com>
2017-02-05 17:56:28 +0200

    Additional service methods. Additional methods for saving, getting all the books from another module, book deletion and finding a book by a specific parameter.

--- main-app/src/main/java/ee/ttu/spring/rest/engine/BookService.java
+++ main-app/src/main/java/ee/ttu/spring/rest/engine/BookService.java
@@ -3,8 +3,11 @@ package ee.ttu.spring.rest.engine;
 import ee.ttu.spring.rest.domain.Book;
 import ee.ttu.spring.rest.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -21,7 +24,29 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-    public void saveNewBook(Book newBook) {
-        bookRepository.save(newBook);
+    public void saveBook(Book book) {
+        bookRepository.save(book);
     }
+
+    public List<Book> getAllBooksFromTransmitterApplication() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Book[]> responseEntity = restTemplate.getForEntity("http://localhost:9500/book-ext/get-all", Book[].class);
+        List<Book> externalBooks = Arrays.asList(responseEntity.getBody());
+
+        return externalBooks;
+    }
+
+    public void removeBook(Book bookToRemove) {
+        bookRepository.delete(bookToRemove);
+    }
+
+    public List<Book> findByParameter(String searchParameter, String parameterValue) {
+        if (searchParameter.equals("genre")) {
+            return bookRepository.findByGenre(parameterValue);
+        } else if (searchParameter.equals("authorsNames")){
+            return bookRepository.findByAuthorsNames(parameterValue);
+        } else {
+            return bookRepository.findByBookTitle(parameterValue);
+        }
+}
 }
