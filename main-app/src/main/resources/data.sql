09678a64d465aaf6e94a133773be9fa00016513f
hansmu <hans.miikael@gmail.com>
2017-02-05 17:53:28 +0200

    Comma-separated list for authors instead of a separate table for easier searching and to comply with homework specifications.

--- main-app/src/main/resources/data.sql
+++ main-app/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-MERGE INTO book (id, book_title, genre, page_count) VALUES (1, 'Greatest Book', 'Mystery', 273);
-MERGE INTO book (id, book_title, genre, page_count) VALUES (2, 'Also Good Book', 'Adventure', 353);
-MERGE INTO authors (id, authors_names, book_id) VALUES (1, 'Bob Roberts', 1);
-MERGE INTO authors (id, authors_names, book_id) VALUES (2, 'Michael J. Flackson', 1);
-MERGE INTO authors (id, authors_names, book_id) VALUES (3, 'John J. Johnathan', 2);
+MERGE INTO book (id, authors_names, book_title, genre, page_count) VALUES (1, 'Bob Roberts, Michael J. Flackson', 'Greatest Book', 'Mystery', 273);
+MERGE INTO book (id, authors_names,book_title, genre, page_count) VALUES (2, 'John J. Johnathan', 'Also Good Book', 'Adventure', 353);
+
--- main-app/src/main/resources/schema.sql
+++ main-app/src/main/resources/schema.sql
@@ -1,18 +1,10 @@
-DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS book;
 
 CREATE TABLE book
 (
   id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  authors_names VARCHAR2(2000),
   book_title VARCHAR2(250),
   genre VARCHAR2(250),
   page_count INTEGER
-);
-
-CREATE TABLE authors
-(
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  authors_names VARCHAR2(250),
-  book_id BIGINT,
-  FOREIGN KEY (book_id) REFERENCES book(id)
 );
