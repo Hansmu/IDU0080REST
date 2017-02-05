bca2120d1136550a515ba11fd68e68c7c6bb9388
hansmu <hans.miikael@gmail.com>
2017-02-05 17:58:54 +0200

    Additional repository queries for searching New methods to query by book title, genre or authors' names.

--- main-app/src/main/java/ee/ttu/spring/rest/repository/BookRepository.java
+++ main-app/src/main/java/ee/ttu/spring/rest/repository/BookRepository.java
@@ -2,8 +2,34 @@ package ee.ttu.spring.rest.repository;
 
 import ee.ttu.spring.rest.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    @Query("SELECT b " +
+            "FROM Book b " +
+            "WHERE lower(b.bookTitle) LIKE concat('%', lower(:title), '%') " +
+            "OR lower(b.bookTitle) LIKE concat('%', lower(:title)) " +
+            "OR lower(b.bookTitle) LIKE concat(lower(:title), '%')")
+    List<Book> findByBookTitle(@Param(value="title")String bookTitle);
+
+    @Query("SELECT b " +
+            "FROM Book b " +
+            "WHERE lower(b.genre) LIKE concat('%', lower(:genre), '%') " +
+            "OR lower(b.genre) LIKE concat('%', lower(:genre)) " +
+            "OR lower(b.genre) LIKE concat(lower(:genre), '%')")
+    List<Book> findByGenre(@Param(value="genre")String genre);
+
+    @Query("SELECT b " +
+            "FROM Book b " +
+            "WHERE lower(b.authorsNames) LIKE concat('%', lower(:authorsNames), '%') " +
+            "OR lower(b.authorsNames) LIKE concat('%', lower(:authorsNames)) " +
+            "OR lower(b.authorsNames) LIKE concat(lower(:authorsNames), '%')")
+    List<Book> findByAuthorsNames(@Param(value="authorsNames")String authorsNames);
+
 }
